ac66d9e5db76b25d66073aa839b2c9a85644e2f1
Jordan Ward <jordancward@gmail.com>
2024-10-01 09:32:16 -0600

    Get data in a dictionary and print the titles and descriptions

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ api_key = "0104be7090ad48f9ad65a4ca468f544a"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-09-01&sortBy=" \
       "publishedAt&apiKey=0104be7090ad48f9ad65a4ca468f544a"
 
+# Make a request
 website = requests.get(url)
-content = website.text
-print(content)
+
+# Get a dictionary with data
+content = website.json()
+
+# Access the article titles and descriptions
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
